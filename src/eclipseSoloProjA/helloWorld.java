d6c44e1f3d6b5c616b84c1eebf9e5c2dbf2ce530
Brandon Huynh <Kudulobrandon@gmail.com>
2024-02-12 17:23:03 -0500

    this will cause conflict yes
    This is definitely a merge conflict

--- src/eclipseSoloProjA/helloWorld.java
+++ src/eclipseSoloProjA/helloWorld.java
@@ -3,9 +3,8 @@ package eclipseSoloProjA;
 public class helloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("test github project");
-		System.out.println("this is the alternate second line. This is a conflict");
+		System.out.println("this is the alternate second line. This is a conflict. This is definitely a conflict watch out");
 	}
 
 }
