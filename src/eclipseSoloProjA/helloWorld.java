ecce86b8b95be15845668a19e1022bef181eb637
Brandon Huynh <Kudulobrandon@gmail.com>
2024-02-12 17:01:49 -0500

    Merge conflict

--- src/eclipseSoloProjA/helloWorld.java
+++ src/eclipseSoloProjA/helloWorld.java
@@ -5,7 +5,7 @@ public class helloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test github project");
-		System.out.println("this is the alternate second line");
+		System.out.println("this is the alternate second line. This is a conflict");
 	}
 
 }
